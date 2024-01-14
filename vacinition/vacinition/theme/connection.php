264bdcab8a040294ae900101de68a097cf4a8fa9
mehreen-faiz <139073527+mehreen-faiz@users.noreply.github.com>
2024-01-14 15:42:40 -0800

    template css done

---
+++ vacinition/vacinition/theme/connection.php
@@ -0,0 +1,11 @@
+<?php 
+     $host = "localhost";
+     $user = "root";
+     $password = "mehreenfaiz#3";
+     $dbconnect = "vaccinations_system";
+
+     $connection = mysqli_connect($host,$user,$password,$dbconnect);
+
+    //  echo $connection ? "Connected" : "Nor Connected";
+
+?>
